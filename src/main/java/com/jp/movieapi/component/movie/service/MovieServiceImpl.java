a843673beab62899fab5e2e1b044d0ca2233e42a
jp personal <gabutin.jp@gmail.com>
2025-09-21 17:34:07 +0800

    chore: add getMovieById in service and interface

--- src/main/java/com/jp/movieapi/component/movie/service/MovieServiceImpl.java
+++ src/main/java/com/jp/movieapi/component/movie/service/MovieServiceImpl.java
@@ -1,9 +1,12 @@
 package com.jp.movieapi.component.movie.service;
 
 import java.util.List;
+import java.util.UUID;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.jp.movieapi.common.domain.ResourceNotFoundException;
 import com.jp.movieapi.component.movie.domain.Movie;
 import com.jp.movieapi.component.movie.domain.MovieRepository;
 
@@ -18,5 +21,14 @@ public class MovieServiceImpl implements MovieService {
     @Override
     public List<Movie> listAllMovies() {
         return movieRepository.findAllMovies();
+    }
+
+    @Override
+    public Movie getMovieById(UUID id) {
+        try {
+            return movieRepository.findMovieById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException("Movie", id);
+        }
     }
 }
